e0c405529cc813f987b5d2a308e98d712cf0fc3a
Siddharth <sid2k01@gmai.com>
2025-11-02 14:38:31 +0000

    update rest_api.py - put method, last one was supposed to be post

--- basics/communication/rest_api.py
+++ basics/communication/rest_api.py
@@ -12,5 +12,13 @@ todo = {"userId": 1, "title": "Buy Milk", "completed": False}
 post_response = requests.post(api_url, json=todo)
 post_response_info = [post_response.json(), post_response.status_code, post_response.headers["Content-Type"]]
 
+# PUT Method.
+api_url = "https://jsonplaceholder.typicode.com/todos/10"
+response = requests.get(api_url)
+print(response.json())
+todo = {"userId": 1, "title": "Wash Car", "completed": True}
+response = requests.put(api_url, json=todo)
+print(response.json())
+
 with open("post_response.json", "w", encoding="utf-8") as write_file:
     json.dump(post_response_info, write_file, indent=2)
