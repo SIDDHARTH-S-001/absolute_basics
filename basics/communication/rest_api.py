c3f75eebd91eb22754b4df18476adc19deb65031
Siddharth <sid2k01@gmai.com>
2025-11-02 14:30:49 +0000

    update: rest_api.py - add PUT method

--- basics/communication/rest_api.py
+++ basics/communication/rest_api.py
@@ -1,7 +1,16 @@
 import requests
 import json
 
+# GET Method.
 api_url = "https://jsonplaceholder.typicode.com/todos/1"
-response = requests.get(api_url).json()
-with open("response.json", "w", encoding="utf-8") as write_file:
-    json.dump(response, write_file)
+get_response = requests.get(api_url)
+get_response_info = [get_response.json(), get_response.status_code, get_response.headers["Content-Type"]]
+
+# POST Method.
+api_url = "https://jsonplaceholder.typicode.com/todos"
+todo = {"userId": 1, "title": "Buy Milk", "completed": False}
+post_response = requests.post(api_url, json=todo)
+post_response_info = [post_response.json(), post_response.status_code, post_response.headers["Content-Type"]]
+
+with open("post_response.json", "w", encoding="utf-8") as write_file:
+    json.dump(post_response_info, write_file, indent=2)
