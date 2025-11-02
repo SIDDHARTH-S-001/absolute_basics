bd76814f78d59312e81aac9f70f856fcd3c92788
Siddharth <sid2k01@gmai.com>
2025-11-02 14:16:13 +0000

    update: rest_api.py - save response to a json file

--- basics/communication/rest_api.py
+++ basics/communication/rest_api.py
@@ -1,4 +1,7 @@
 import requests
+import json
+
 api_url = "https://jsonplaceholder.typicode.com/todos/1"
-response = requests.get(api_url)
-print(response.json())
+response = requests.get(api_url).json()
+with open("response.json", "w", encoding="utf-8") as write_file:
+    json.dump(response, write_file)
