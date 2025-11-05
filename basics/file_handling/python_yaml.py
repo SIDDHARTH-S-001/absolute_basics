74860013566e69bd1e91a45819241d338aa1137b
Siddharth <sid2k01@gmai.com>
2025-11-05 22:57:27 +0000

    refactor: python_yaml.py - wrapped pyyaml implementation into a class with member functions

--- basics/file_handling/python_yaml.py
+++ basics/file_handling/python_yaml.py
@@ -1,29 +1,35 @@
 import yaml
 import numpy as np
 
-def read_yaml(filename):
-    with open(filename) as file:
-        try: 
-            data = yaml.safe_load(file)
-            return data
-        except yaml.YAMLError as e:
-            print(e)
+class UsingPyyaml():
+    def __init__(self):
+        pass
 
-def write_yaml(filename, data):
-    with open(filename, mode="w") as write_file:
-        output = yaml.dump(data, write_file)
-        return output
+    def read_yaml(self, filename):
+        with open(filename) as file:
+            try: 
+                data = yaml.safe_load(file)
+                return data
+            except yaml.YAMLError as e:
+                print(e)
 
-filename = "random-data.yaml"
-# data = read_yaml(filename)
-data = {
-    "name": "random_name",
-    "address": "random_address", 
-    "age": np.random.randint(18, 25),
-    "stack": ["systems engineering", "reinforcement learning", "computer vision", "simulations"]
-}
-print(data)
-print(type(data))
-for key, value in data.items():
-    print(f"{key}: {value}")
-output = write_yaml(filename=filename, data=data)
+    def write_yaml(self, filename, data):
+        with open(filename, mode="w") as write_file:
+            output = yaml.dump(data, write_file)
+            return output
+        
+if __name__ == "__main__":
+    up = UsingPyyaml()
+    filename = "random-data.yaml"
+    # data = read_yaml(filename)
+    data = {
+        "name": "random_name",
+        "address": "random_address", 
+        "age": np.random.randint(18, 25),
+        "stack": ["systems engineering", "reinforcement learning", "computer vision", "simulations"]
+    }
+    print(data)
+    print(type(data))
+    for key, value in data.items():
+        print(f"{key}: {value}")
+    output = up.write_yaml(filename=filename, data=data)
