247103c9dd2d7037b5f833bdd2e934d181eb067c
Siddharth <sid2k01@gmai.com>
2025-11-05 23:45:38 +0000

    feat: utilized hydra & omegaconf - loading yaml file & parsing its params

--- basics/file_handling/python_yaml.py
+++ basics/file_handling/python_yaml.py
@@ -1,5 +1,7 @@
 import yaml
 import numpy as np
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
 class UsingPyyaml():
     def __init__(self):
@@ -18,18 +20,32 @@ class UsingPyyaml():
             output = yaml.dump(data, write_file)
             return output
         
+def parse_cfg(cfg):
+    app, db, feat, log = cfg.app, cfg.database, cfg.features, cfg.logging
+    print(app, db, feat, log)
+    print(type(app))
+    print(app.name)
+
+@hydra.main(version_base=None, config_path=".", config_name="app-config")
+def main(cfg: DictConfig) -> None:
+    # print(OmegaConf.to_yaml(cfg))
+    parse_cfg(cfg=cfg)
+
 if __name__ == "__main__":
-    up = UsingPyyaml()
-    filename = "random-data.yaml"
-    # data = read_yaml(filename)
-    data = {
-        "name": "random_name",
-        "address": "random_address", 
-        "age": np.random.randint(18, 25),
-        "stack": ["systems engineering", "reinforcement learning", "computer vision", "simulations"]
-    }
-    print(data)
-    print(type(data))
-    for key, value in data.items():
-        print(f"{key}: {value}")
-    output = up.write_yaml(filename=filename, data=data)
+    # up = UsingPyyaml()
+    # filename = "random-data.yaml"
+    # # data = read_yaml(filename)
+    # data = {
+    #     "name": "random_name",
+    #     "address": "random_address", 
+    #     "age": np.random.randint(18, 25),
+    #     "stack": ["systems engineering", "reinforcement learning", "computer vision", "simulations"]
+    # }
+    # print(data)
+    # print(type(data))
+    # for key, value in data.items():
+    #     print(f"{key}: {value}")
+    # output = up.write_yaml(filename=filename, data=data)
+    data = main()
+    # print(data.weekday)
+    type(data)
