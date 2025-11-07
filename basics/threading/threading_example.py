ac117335618a5431ba963a9db673500059105349
Siddharth <sid2k01@gmai.com>
2025-11-07 13:23:25 +0000

    update: threading_example.py - Threading as a sub-class

--- basics/threading/threading_example.py
+++ basics/threading/threading_example.py
@@ -11,6 +11,17 @@ def thread_delay(thread_name, delay):
 def cube_volume(a):
     print(f"Volume of cube = {a*a*a}")
 
+class ThreadClass(threading.Thread):
+    def __init__(self, name, delay):
+        threading.Thread.__init__(self)
+        self.delay = delay
+
+    def run(self):
+        print("Starting thread: ", self.name)
+        thread_delay(self.name, self.delay)
+        print("Execution of Thread: ", self.name, "is complete!")
+
+###### Example 1: Basic Example
 # t1 = threading.Thread(target=thread_delay, args=("t1", 1))
 # t2 = threading.Thread(target=thread_delay, args=("t2", 3))
 
@@ -24,10 +35,20 @@ def cube_volume(a):
 # t2.join() 
 # print("This will execute only after t1 completes")
 
+###### Example 2: args as an interable
 # t1 = threading.Thread(target=cube_volume, args=(2)) # will raise an error, as args must be a interable (list / tuple) not a single value.
-t1 = threading.Thread(target=cube_volume, args=(2, )) # add a "," to make it a tuple. This will work.
+# t1 = threading.Thread(target=cube_volume, args=(2, )) # add a "," to make it a tuple. This will work.
+# t1.start()
+# t1.join()
+# print("Process complete")
+
+###### Example 3: Threading as a sub-class.
+t1 = ThreadClass("t1", 1)
+t2 = ThreadClass("t2", 3)
 t1.start()
+t2.start()
 t1.join()
-print("Process complete")
+t2.join()
+print("Execution Complete")
 
 
