cca05c3083192919b19bca5de409d264d5fdaaf0
Siddharth <sid2k01@gmai.com>
2025-11-07 13:15:30 +0000

    update: threading_example.py - pass args as iterable

--- basics/threading/threading_example.py
+++ basics/threading/threading_example.py
@@ -8,17 +8,26 @@ def thread_delay(thread_name, delay):
         count += 1
         print(f"Thread {thread_name} -----> {time.time()}")
 
-t1 = threading.Thread(target=thread_delay, args=("t1", 1))
-t2 = threading.Thread(target=thread_delay, args=("t2", 3))
+def cube_volume(a):
+    print(f"Volume of cube = {a*a*a}")
 
-t1.start() # 'start' method starts the thread & must be called 'at most' once for a thread.
-t2.start() # 'start' method will raise RUNTIMEERROR if called multiple times.
+# t1 = threading.Thread(target=thread_delay, args=("t1", 1))
+# t2 = threading.Thread(target=thread_delay, args=("t2", 3))
 
-input("Press Enter to interrupt threads")
-print("Threads interrupted, but they'll continue until they finish")
+# t1.start() # 'start' method starts the thread & must be called 'at most' once for a thread.
+# t2.start() # 'start' method will raise RUNTIMEERROR if called multiple times.
 
-t1.join() # blocks main thread until the thread t1 is finished if timeout=None. Can set a timeout as well.
-t2.join() 
-print("This will execute only after t1 completes")
+# input("Press Enter to interrupt threads")
+# print("Threads interrupted, but they'll continue until they finish")
+
+# t1.join() # blocks main thread until the thread t1 is finished if timeout=None. Can set a timeout as well.
+# t2.join() 
+# print("This will execute only after t1 completes")
+
+# t1 = threading.Thread(target=cube_volume, args=(2)) # will raise an error, as args must be a interable (list / tuple) not a single value.
+t1 = threading.Thread(target=cube_volume, args=(2, )) # add a "," to make it a tuple. This will work.
+t1.start()
+t1.join()
+print("Process complete")
 
 
