b2c22028654126b9738222e13d69756ad08212ee
Siddharth <sid2k01@gmai.com>
2025-11-07 13:06:30 +0000

    add: threading_example.py - basic implementation

---
+++ basics/threading/threading_example.py
@@ -0,0 +1,24 @@
+import threading
+import time
+
+def thread_delay(thread_name, delay):
+    count = 0
+    while count < 3:
+        time.sleep(delay)
+        count += 1
+        print(f"Thread {thread_name} -----> {time.time()}")
+
+t1 = threading.Thread(target=thread_delay, args=("t1", 1))
+t2 = threading.Thread(target=thread_delay, args=("t2", 3))
+
+t1.start() # 'start' method starts the thread & must be called 'at most' once for a thread.
+t2.start() # 'start' method will raise RUNTIMEERROR if called multiple times.
+
+input("Press Enter to interrupt threads")
+print("Threads interrupted, but they'll continue until they finish")
+
+t1.join() # blocks main thread until the thread t1 is finished if timeout=None. Can set a timeout as well.
+t2.join() 
+print("This will execute only after t1 completes")
+
+
