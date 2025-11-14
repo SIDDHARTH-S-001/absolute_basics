a03c3f9dc97636f444ca3d8ff05c1a55db17ab03
Siddharth <sid2k01@gmai.com>
2025-11-14 23:50:01 +0000

    add: numpy_tut_1.py - array fundamentals complete

---
+++ basics/advanced_python/numpy_tut_1.py
@@ -0,0 +1,44 @@
+import numpy as np
+import math
+
+""" Numpy array rules.
+All elements of the array must be of the same type of data.
+
+Once created, the total size of the array can't change.
+
+The shape must be “rectangular”, not “jagged”; e.g., each row of a two-dimensional array must have the same number of columns.
+
+"""
+
+def array_fundamentals():
+    # a = np.array([[1, 2, 3], 
+    #               [4, 5, 6]])
+
+    a = np.array([1, 2, 3, 4, 5, 6])
+    print(a) # (2, 3)
+    print(a.shape)
+    b = a[:3]
+    print(b)
+    b = a[3:]
+    print(b)
+    b[0] = 80 # numpy ndarrays make copies instead of views (python lists create views).
+    # this means when "b" is changed, even "a" changes.
+    print(b)
+    print(a)
+
+    # The number of dimensions of an array is contained in the ndim attribute.
+    print(a.ndim)
+
+    # The shape of an array is a tuple of non-negative integers that specify the number of elements along each dimension.
+    print(a.shape)
+    print(len(a.shape) == a.ndim)
+
+    # The fixed, tota number of elements in array s contained in the size attribute.
+    print(a.size)
+    print(a.size == math.prod(a.shape))
+
+    # The arrays are homogeneous, meaning they only contain elements of 1 datatype. 
+    print(a.dtype)
+
+if __name__ == "__main__":
+    array_fundamentals()
