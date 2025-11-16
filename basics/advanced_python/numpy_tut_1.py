a9dc4a55d353a4ae71dfae84c72fc20cbc5c37ec
Siddharth <sid2k01@gmai.com>
2025-11-16 20:07:18 +0000

    update: numpy tut 1 - add array_operations

--- basics/advanced_python/numpy_tut_1.py
+++ basics/advanced_python/numpy_tut_1.py
@@ -8,7 +8,7 @@ Once created, the total size of the array can't change.
 
 The shape must be “rectangular”, not “jagged”; e.g., each row of a two-rdimensional array must have the same number of columns.
 
-"""
+""" 
 
 def array_fundamentals():
     # a = np.array([[1, 2, 3], 
@@ -49,6 +49,63 @@ def basic_arrays():
     print(np.linspace(0, 10, num=5)) # create an array with values that are spaced linearly in a specified interval.
     print(np.ones(2, dtype=np.int64)) # explictly mention the data-type.
 
+def array_operations():
+    arr = np.array([2, 1, 5, 3, 7, 4, 6, 8])
+    arr_asc = np.sort(arr) # sorts the array in ascending order.
+    print(arr, "\n", arr_asc)
+    a = np.array([1, 2, 3, 4])
+    b = np.array([5, 6, 7, 8, 9])
+    print(np.concatenate((a, b))) # "concat" can join only two things while "concatenate" can join two or more things.
+    x = np.array([[1, 2], [3, 4]])
+    y = np.array([[5, 6]])
+    print(np.concatenate((x, y), axis=0))
+    # print(np.concatenate((x, y), axis=1)) # This will throw an error, because all the input array dimensions except for the concatenation axis must match exactly.
+    c =  np.arange(6)
+    print(c.reshape((2, 3)))
+    print(np.reshape(c, (3, 2), order='C'))
+    # C means to read/write the elements using C-like index order.
+    # F means to read/write the elements using Fortran-like index order.
+    # A means to read/write the elements in Fortran-like index order if a is Fortran contiguous in memory, C-like order otherwise. 
+    # In Fortran, the first index changes rapidly & in C, the last index changes rapidly.
+
+    d = np.array([2, 4, 6, 8, 10])
+    d1 = d[np.newaxis, : ] # row-vector.
+    d2 = d[:, np.newaxis]  # column-vector.
+    print(d1.shape, d2.shape)
+    d3 = np.expand_dims(d, axis=1) # adds an extre trailing dimension.
+    d4 = np.expand_dims(d, axis=0) # adds an extra leading dimension.
+    print(d3.shape, d4.shape)
+
+    e = np.array([[1, 2, 3, 4], [5, 6, 7 ,8], [9, 10, 11, 12]])
+    print(e[e < 5])
+    five_up = (e > 5)
+    print(e[five_up])
+    print(e[e % 2 == 0]) # divisible by 2.
+    print(e[(e > 5) & (e % 2 == 0)]) # stackng conditions using &. /
+    five_and_up = (e > 5) | (e == 5) # stores boolean value.
+    print(five_and_up)
+
+    f1 = np.array([[1, 1], [2, 2]])
+    f2 = np.array([[3, 3], [4, 4]])
+    print(np.vstack((f1, f2))) # horizontal stack.
+    print(np.hstack((f1, f2))) # vertical stack.
+    f = np.arange(1, 25).reshape(2, 12)
+    print(np.hsplit(f, 3)) # splits the array into 3 equal parts.
+    print(np.hsplit(f, (3, 4))) # split array after 3rd & 4th element.
+    f_copy = f.copy() # creates a complete copy.
+
+    data = np.array([1, 2])
+    ones = np.ones(2)
+    print(data + ones) # adds elements
+    print(data - ones) # subtracts elements
+    print(data * ones) # multiplies elements
+    print(ones / data) # divides elements
+    print(sum(np.array([1, 3, 7, 12]))) # sum of elements 
+    h = np.array([[1, 2], [3, 4]])
+    print(h.sum(axis=0)) # sum along row.
+    print(h.sum(axis=1)) # sum along column.
+
 if __name__ == "__main__":
     # array_fundamentals()
-    basic_arrays()
+    # basic_arrays()
+    array_operations()
