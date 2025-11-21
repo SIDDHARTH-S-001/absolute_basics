170c668d03c66580a908016b0f7ee0ebd6edda03
Siddharth <sid2k01@gmai.com>
2025-11-21 14:31:32 +0000

    add: test_fixtures.py

---
+++ basics/testing/pytest_tutorials/test_fixtures.py
@@ -0,0 +1,23 @@
+import pytest
+
+@pytest.fixture
+def input_value(): # fixture function.
+    input = 39
+    return input
+
+@pytest.mark.divisible
+def test_divisible_by_3(input_value): # The fixture funcion must be the input parameter.
+    assert input_value % 3 == 0 # value returned by the fixture function is stored in the variable input_value.
+
+@pytest.mark.divisible
+def test_divisible_by_6(input_value):
+    assert input_value % 6 == 0
+
+"""
+### Notes ###
+------------------------------------------------------------------------------------------------------------------------
+A fixture function defined inside a test file has a scope within the test file only. 
+We cannot use that fixture in another test file. 
+To make a fixture available to multiple test files, we have to define the fixture function in a file called conftest.py. 
+conftest.py is explained in the next chapter.
+"""
