0fe51ded93ddda9b853a77a3c8c66f3c53d2cf19
S.Siddharth <sid2k01@gmail.com>
2026-09-01 16:11:46 +0530

    update: dsa_utils.py - add Queue class - list format

--- basics/advanced_python/dsa/dsa_utils.py
+++ basics/advanced_python/dsa/dsa_utils.py
@@ -1,8 +1,5 @@
 class Stack:
-    """
-    Implementing Stack as a custom python class gives better encapsulation. 
-    Notes covered in `data_structures.ipynb`. 
-    """
+    "Stack class implemented as a list"
     def __init__(self):
         self.stack = [] # stack implemented as a python list
 
@@ -10,15 +7,18 @@ class Stack:
         return len(self.stack) == 0
 
     def push(self, element):
+        "Adds new element to the top of the stack"
         self.stack.append(element)
 
     def pop(self):
+        "Removes the last element from the stack"
         try:
             self.stack.pop()
         except IndexError:
             print("Stack Empty!")
 
     def peek(self):
+        "Shows the last element of the stack"
         try:
             return self.stack[-1]
         except IndexError:
@@ -27,10 +27,34 @@ class Stack:
     def size(self):
         return len(self.stack)
 
-    
-            
-    
+class Queue:
+    """
+    Queue class implemented as a list
+    """
+    def __init__(self):
+        self.queue = []
 
+    def isEmpty(self):
+        return len(self.queue) == 0
 
+    def enqueue(self, element):
+        "Adds new element to the end of the queue"
+        self.queue.append(element)
 
+    def dequeue(self):
+        "Removes the first element from the queue"
+        try: 
+            self.queue.pop(0) # removes the first entry
+        except IndexError:
+            print("Queue Empty!")
+
+    def peek(self):
+        "Shows first element of the queue"
+        try:
+            self.queue[0]
+        except IndexError:
+            print("Queue Empty!")
+
+    def size(self):
+        return len(self.queue)
     
