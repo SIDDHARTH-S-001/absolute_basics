b6c8fa03bf66de1cc54cdc60499c477441315c3e
S.Siddharth <sid2k01@gmail.com>
2026-09-05 17:38:55 +0530

    update: algo-s - add QuickSort

--- basics/advanced_python/dsa/algo_utils.py
+++ basics/advanced_python/dsa/algo_utils.py
@@ -59,4 +59,40 @@ class SelectionSort:
 
         return array
 
-            
+
+class QuickSort:
+    def __init__(self):
+        pass
+
+    # @staticmethod
+    def partition(self, array, low, high):
+        """
+        Receives a sub-array, moves values around, 
+        Swaps the pivot element into the sub-array, and 
+        
+        input:
+            array, low, high
+
+        returns:
+            the index where the next split in sub-arrays happens.
+        """
+        pivot = array[high]
+        i = low - 1
+
+        for j in range(low, high):
+            if array[j] <= pivot:
+                i = i + 1
+                array[i], array[j] = array[j], array[i]
+        array[i + 1], array[high] = array[high], array[i + 1]
+
+        return i + 1
+
+    # @staticmethod
+    def sort(self, array, low=0, high=None):
+        if high is None: high = len(array) - 1
+
+        if low < high:
+            pivot_index = self.partition(array, low, high)
+            self.sort(array, low, pivot_index - 1)
+            self.sort(array, pivot_index + 1, high)
+
